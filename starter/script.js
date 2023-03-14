8a876b141ac7a5b4f8b8d2e98dc7e30b8f9d8a54
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-03-14 13:49:43 +0100

    add

--- starter/script.js
+++ starter/script.js
@@ -1,13 +1,22 @@
 'use strict';
 
-// function declaration
-function calcAge1(birthYear){
-    return 2037 - birthYear;
-}
-console.log(calcAge1(2000));
+// // function declaration
+// function calcAge1(birthYear){
+//     return 2037 - birthYear;
+// }
+// console.log(calcAge1(2000));
 
-// function expression
+// // function expression
+// const calcAge2 = function(birthYear){
+//     return 2037 - birthYear;
+// }
+// console.log(calcAge2(2001));
+
 const calcAge2 = function(birthYear){
-    return 2037 - birthYear;
-}
-console.log(calcAge2(2001));
+         return 2037 - birthYear;
+     }
+
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+
+console.log(calcAge3(2002));
