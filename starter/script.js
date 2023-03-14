71f20521e9c682663d2f0d8e641fb1ca51f86770
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-03-14 13:30:04 +0100

    add

--- starter/script.js
+++ starter/script.js
@@ -1,10 +1,22 @@
 'use strict';
 
 function logger() {
-    console.log("Hello this course is very bored");
+    console.log("Hello this course is very boring!");
 }
 
 logger();
 logger();
 logger();
 
+function fruitProcessor(apples, oranges){
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice;
+}
+
+// ne retourne que le console log
+fruitProcessor(5,8);
+
+// pour avoir le jus, il faut le stocher dans un verre
+var glace = fruitProcessor(5,8);
+console.log(glace);
