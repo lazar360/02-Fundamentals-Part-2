c8f78f1f1466e503cb39460dce8eaef6ea4fec56
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-03-17 13:39:30 +0100

    add

--- starter/script.js
+++ starter/script.js
@@ -6,10 +6,11 @@ const calcAge = function(birthYeah){
 
 
 const yearsUntilRetirement = function(birthYeah, firstName) {
-    const age = 2037 - calcAge(birthYeah)
+    const age = calcAge(birthYeah)
     const retirement = 65 - age;
-    return retirement;
+    return retirement > 0 ? retirement : -1;
     //return `${firstName} retires in ${retirement} years`;
   }
-  console.log(yearsUntilRetirement(1991, 'Jonas')); console.log(yearsUntilRetirement(1980, 'Bob'));
+  console.log(yearsUntilRetirement(1991, 'Jonas')); 
+  console.log(yearsUntilRetirement(1970, 'Bob'));
   
