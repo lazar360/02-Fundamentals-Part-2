fa0d049d69c071713463addee8b540d5213a0250
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-03-19 11:57:39 +0100

    add

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,23 @@
 'use strict';
+
+// créer un tableau
 const friends = ['Michael', 'John', 'Peter'];
+const years = new Array(1991, 2000, 2022);
+
+// accéder au tableau
+console.log(friends[0]);
+console.log(friends[2]);
+
+// taille du tableau
+console.log(friends.length);
+
+// expression
+console.log(friends[friends.length - 1]);
+
+// remplacer un élément du tableau
+friends[2] = 'Jay';
 console.log(friends);
 
-const years = new Array(1991, 2000, 2022);
+
+
+
