aaab2360534f315ea25629b1cb74735dc11d8527
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-03-19 11:49:32 +0100

    add

--- starter/script.js
+++ starter/script.js
@@ -1,25 +1,3 @@
 'use strict';
-//1. Create a arrow function 'calcAverage' to calculate the average of 3 scores
-const calcAverage = (a,b,c) => (a + b + c)/3;
-
-let DolphinsScore1 = 44, 
-DolphinsScore2 =  23,
-DolphinsScore3 = 71;
-
-let KoalasScore1 = 65,
-KoalasScore2 = 54,
-KoalasScore3 = 49;
-//console.log(`${calcAverage(DolphinsScore1, DolphinsScore2, DolphinsScore3)}`)
-
-const averageDolphinScore = calcAverage(DolphinsScore1, DolphinsScore2, DolphinsScore3);
-const averageKoalaScore = calcAverage(KoalasScore1, KoalasScore2, KoalasScore3);
-
-function checkWinner(averageDolphinScore, averageKoalaScore) {
-
-    const winner = averageDolphinScore > averageKoalaScore ? "Dolphin" : "Koala";
-    console.log(`The winner is ${winner}.`)
-
-}
-
-checkWinner(averageDolphinScore, averageKoalaScore);
-
+const friends = ['Michael', 'John', 'Peter'];
+console.log(friends);
