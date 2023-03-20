254a9451c89d42c640e28e89723f5740ad5e9bca
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-03-20 13:48:25 +0100

    add

--- starter/script.js
+++ starter/script.js
@@ -1,27 +1,12 @@
 'use strict';
 
+const jonas = {
+    firstName : 'Jonas',
+    lastName : 'Schmedtmann',
+    age : 2037 - 1991,
+    job : 'teacher',
+    friends :['John', 'Michael', 'Steven']
+};
 
-const friends = ['Michael', 'John', 'Peter'];
-
-// Ajoute une entrée à la fin de la liste
-friends.push('Jay');
-
-// Ajoute une entrée au début de la liste
-friends.unshift('John');
-
-// Enlève un élément à la fin de la liste
-friends.pop();
-
-// Enlève un élément au début de la liste
-friends.shift();
-
-// Indique l'index de la liste
-console.log(friends.indexOf('John'));
-
-// Retourne true si l'élément est dans la liste
-console.log(friends.includes('John'));
-friends.push(42);
-console.log(friends.includes('42'));// égalité stricte donc retourne false
-
-
-
+console.log(`Hello ! My firste name is ${jonas.firstName}.
+I have a lot of friends : ${jonas.friends}`);
