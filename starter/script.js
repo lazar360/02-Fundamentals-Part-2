f324d9941843dad66930c5f314b41d71cb161f70
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-03-23 17:08:13 +0100

    add

--- starter/script.js
+++ starter/script.js
@@ -8,17 +8,11 @@ const jonas = {
     friends :['John', 'Michael', 'Steven'],
     hasDriversLicence : true,
 
-    // calcAge: function(birthYeah) {
-    //     return 2037 - birthYeah;
-    // }
-
     calcAge: function() {
-        console.log(this);
-        return 2037 - this.birthYeah;
+        this.age = 2037 - this.birthYeah;
+        return this.age;
     }
 };
 
-console.log(jonas.calcAge(2000));
-// console.log(jonas['calcAge'](2000));
 
 
