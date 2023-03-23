ea46674b7fb77652ee62cd07bd7b1732c60051d3
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-03-23 17:40:03 +0100

    add

--- starter/script.js
+++ starter/script.js
@@ -11,8 +11,12 @@ const jonas = {
     calcAge: function() {
         this.age = 2037 - this.birthYeah;
         return this.age;
+    },
+
+    summary: function() {
+        console.log(`${this.firstName} is a ${this.calcAge()} years old ${this.job} and ${this.hasDriversLicence ?' he has a driver license' : ' he doesn\'t have a driver license'}`)  
     }
 };
 
-
+jonas.summary();
 
