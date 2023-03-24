94ea3658cbbc978e4760fc36733ae3486827441d
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-03-24 14:01:35 +0100

    add

--- starter/script.js
+++ starter/script.js
@@ -1,16 +1,5 @@
 'use strict';
 
-// console.log('Lifting weights repetition 1');
-// console.log('Lifting weights repetition 2');
-// console.log('Lifting weights repetition 3');
-// console.log('Lifting weights repetition 4');
-// console.log('Lifting weights repetition 5');
-// console.log('Lifting weights repetition 6');
-// console.log('Lifting weights repetition 7');
-// console.log('Lifting weights repetition 8');
-// console.log('Lifting weights repetition 9');
-// console.log('Lifting weights repetition 10');
-
 for(let i = 1; i <= 10; i++){
     console.log(`Lifting weights repetition ${i}`);
 }
