382ae553b1fbe6fe3e2785448f3e4ec168a2570c
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-03-25 19:29:09 +0100

    Backwards loop

--- starter/script.js
+++ starter/script.js
@@ -6,21 +6,12 @@ const jonasArray = [
     2037 - 1991,
     'teacher',
     ['Michale', 'Peters', 'Steven'],
-    true
 ];
  
-// continue and break the loop
-console.log('ONLY STRING');
-for(let i = 0; i < jonasArray.length; i++){
-    if(typeof jonasArray[i] !== "string") continue;
+for(let i = jonasArray.length-1; i >= 0; i--){
     console.log(jonasArray[i]);
 }
 
-console.log('BREAK WITH NUMBER');
-for(let i = 0; i < jonasArray.length; i++){
-    if(typeof jonasArray[i] === "number") break;
-    console.log(jonasArray[i]);
-}
 
 
 
