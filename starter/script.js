fddba404f3ecf88072d466cc763fef750d4c32eb
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-03-27 13:26:32 +0200

    passage à developers skills

--- starter/script.js
+++ starter/script.js
@@ -1,12 +1,5 @@
 'use strict';
 
-let dice = Math.trunc(Math.random()*6)+1;
-
-while(dice !== 6){
-     console.log(`You roll a ${dice} dice`);
-     dice = Math.trunc(Math.random()*6)+1;
- }
- console.log(`You roll a  6 dice`); 
 
 
 
