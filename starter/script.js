6d4498d5c424c63baa93fa9ff33b00d1ec80f610
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-03-30 09:13:32 +0200

    parcourir un objet en js

--- starter/script.js
+++ starter/script.js
@@ -1,10 +1,16 @@
-'use strict';
+"use strict";
+const monSuperObjet = {
+    cquoi : "chaise",
+    oucest : "au bureau",
+    pourquoifaire : "s'assoir"
+    }
 
+console.log(">>>>>> on parcourt les propriétés de l'objet");
+for(var element in monSuperObjet){
+    console.log(element);
+}
 
-
-
-
-
-
-
-
+console.log(">>>>>>>>> on parcourt les valeurs dans les propriétés de l'objet");
+for(var element in monSuperObjet){
+    console.log(monSuperObjet[element]);
+}
