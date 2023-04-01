202c28b24bfc859fd37356b8177158a2f0b628ee
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-04-01 19:26:49 +0200

    add

--- starter/script.js
+++ starter/script.js
@@ -3,9 +3,9 @@ const measureKelvin = function () {
   const measurement = {
     type: "temp",
     unit: "celsius",
-    value: prompt("Degrees Celsius :")
+    value: prompt("Degrees Celsius :"),
   };
-  const kelvin = measurement.value + 273;
-  return kelvin
+  const kelvin = Number(measurement.value) + 273;
+  return kelvin;
 };
 console.log(measureKelvin());
